21076bb2da5cb4d0052f9f5c01dcb22d10b9f8a2
rmill <rmiller003@gmail.com>
2020-12-31 14:14:52 -0500

    Step 2

--- weather.py
+++ weather.py
@@ -3,6 +3,35 @@
 
 import tkinter as tk
 
+HEIGHT = 500
+WIDTH = 600
+
+def test_function():
+    print("Button clicked")
+
 root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='clouds.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", font=40, command=test_function)
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+lable = tk.Label(lower_frame)
+lable.place(relwidth=1, relheight=1)
+
 
 root.mainloop()
