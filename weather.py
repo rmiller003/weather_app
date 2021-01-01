a6b1e1cac1b64fc3682b880dd5c7d0deee42b7cc
rmill <rmiller003@gmail.com>
2020-12-31 23:07:28 -0500

    Done

--- weather.py
+++ weather.py
@@ -5,17 +5,23 @@
 # api.openweathermap.org/data/2.5/forecast?q={city name}, {country code}
 
 import tkinter as tk
+from tkinter import font
 import requests
 
 HEIGHT = 500
 WIDTH = 600
 
 def format_response(weather):
-    name = weather['name']
-    desc = weather['weather'][0]['description']
-    temp = weather['main']['temp']
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
 
-    return str(name) + ' ' + str(desc) + ' ' + str(temp)
+        final_str = 'City: %s \nCondition: %s \nTemperature (°C): %s' % (name, desc, temp)
+    except:
+        final_str = 'There was a problem retrieving that information'
+
+    return final_str
 
 def test_function(entry):
     print("This is the entry:", entry)
@@ -34,23 +40,23 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
 canvas.pack()
 
-background_image = tk.PhotoImage(file='clouds.png')
+background_image = tk.PhotoImage(file='2.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-entry = tk.Entry(frame, font=40)
+entry = tk.Entry(frame, font=('courier', 18))
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Weather", font=('courier', 12), command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-lable = tk.Label(lower_frame)
+lable = tk.Label(lower_frame, font=('courier 18'))
 lable.place(relwidth=1, relheight=1)
 
 
